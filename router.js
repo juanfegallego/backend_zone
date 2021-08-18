5126a0e50ba5d10a166361308bf000ee8220c74d
juanfegallego <juanfegallegodj@gmail.com>
2021-08-18 13:39:56 +0200

    event router created

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRouter = require("./routes/userRouter");
+const eventRouter = require("./routes/eventRouter");
 
 router.use("/user", userRouter);
+router.use("/event", eventRouter);
 
 module.exports = router;
---
+++ routes/eventRouter.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const eventController = require("../controllers/eventController");
+const admin = require("../middleware/admin.js");
+const auth = require("../middleware/auth");
+
+//GET - Return all Events in the DB
+
+router.get("/", async (req, res) => {
+  try {
+    res.json(await eventController.findAllEvents());
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+//POST - Creates a new Event in the DB
+//  requeire de el rol de admin *
+router.post("/create",async (req, res) => {
+  try {
+    const event = req.body;
+    res.json(await eventController.createEvent(event));
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+// PUT modify Event //admin*
+router.put("/",admin, async (req, res) => {
+  try {
+    const data = req.body;
+    res.json(await eventController.modifyEvent(data));
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// admin *
+
+router.delete("/:id",admin,async (req, res) => {
+  try {
+    const id = req.params.id;
+    res.json(await eventController.deleteEvent(id));
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+
+module.exports = router;

b91d1afcd76ec45ca018b92545f24069d5d2cffe
juanfegallego <juanfegallegodj@gmail.com>
2021-08-18 13:39:48 +0200

    event controllers created

---
+++ controllers/eventController.js
@@ -0,0 +1,32 @@
+const User = require("../models/users");
+const Event = require("../models/events");
+
+
+class Local {
+
+  async createEvent(event) {
+    return Event.create(event);
+  };
+
+  async findAllEvents() {
+    return Event.find();
+  };
+
+
+  async modifyRest(data) {
+    return Event.findByIdAndUpdate(
+      //Where
+      { _id: data.id },
+       //Data Changes
+      {isActive: data.isActive,},
+
+    );
+  };
+  // delete event//
+  async deleteRest(id) {
+    return Event.findByIdAndRemove({ _id: id });
+  };
+}
+
+let eventController = new Local();
+module.exports = eventController;
